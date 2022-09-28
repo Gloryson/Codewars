4f45a8c26571d6ff3c8a47f0eae2ac26cbc8961f
Władysław Bałkunou <92429825+Gloryson@users.noreply.github.com>
2022-09-28 18:15:00 +0300

    feat: add solution

---
+++ 2 kyu Multi Line Task++: Hello World.js
@@ -0,0 +1,46 @@
+You need to write a function f that returns the string Hello, world!.
+
+Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+Hint: It's possible to complete this in 28 lines only.
+
+
+
+
+42
+[
+z=
+'\
+c\
+o\
+n\
+s\
+t\
+r\
+u\
+c\
+t\
+o\
+r'
+][
+z]
+`
+f=
+z\
+=>
+'\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!'
+`(
+)
